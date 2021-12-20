e4727a8de2ccbac0e5696117c5bc9a5c8dfff216
beconsciousproject <96354566+beconsciousproject@users.noreply.github.com>
2021-12-20 17:24:15 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// open discord in new tab
+function NewDiscordTab() {
+    window.open(
+        "https://discord.gg/xAzSXmWaW9", "_blank"
+    );
+}
+
+// open twitter in new tab
+function NewTwitterTab() {
+    window.open(
+        "https://www.twitter.com", "_blank"
+    );
+}
